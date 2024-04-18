8091919102d75d31a0552d748363b8d77c968538
joyal619 <joyal.jos619@gmail.com>
2024-04-18 13:02:25 +0530

    react init

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Routing from './Components/Router/Routing'
+
+function App() {
+  
+  return (
+    <div className="App">
+     <Routing/>
+     </div>
+  )
+}
+
+export default App
